3168eeb806aa944028217d23cde547f378739843
Itay Sidis <is874k@att.com>
2017-02-21 14:46:26 +0200

    Detecting changes of selected password

--- web/src/app/components/password-detail/password-detail.component.ts
+++ web/src/app/components/password-detail/password-detail.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { Password } from '../../models/password';
 @Component({
   selector: 'password-detail',
   templateUrl: './password-detail.component.html',
   styleUrls: ['./password-detail.component.scss']
 })
 export class PasswordDetailComponent implements OnInit {
-  @Input() selectedPassword;
+
+  private _selectedPassword: Password;
+
+  @Input() set selectedPassword(newPass: Password) {
+    this._selectedPassword = newPass;
+  }
+
+  get selectedPassword(): Password {
+    return this._selectedPassword;
+  }
   constructor() { }
 
   ngOnInit() {
--- web/src/app/components/tree/tree.component.ts
+++ web/src/app/components/tree/tree.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, Output ,EventEmitter } from '@angular/core';
 import { tree } from '../../../assets/mock';
+import { Password } from '../../models/password';
 @Component({
   selector: 'tree',
   templateUrl: './tree.component.html',
   styleUrls: ['./tree.component.scss']
 })
 export class TreeComponent implements OnInit {
-  @Output() onSelectPassword:EventEmitter<any> = new EventEmitter();
-  selectedPassword = null;
+  @Output() onSelectPassword:EventEmitter<Password> = new EventEmitter();
+  selectedPassword: Password = null;
   tree = tree;
   constructor() { }
 
